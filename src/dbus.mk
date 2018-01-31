97be535f76737c3e9ab1fb9326e4f0f1ba512a09
franklee1 <34935403+franklee1@users.noreply.github.com>
2018-01-31 04:15:33 -0800

    Add files via upload

---
+++ src/dbus.mk
@@ -0,0 +1,33 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := dbus
+$(PKG)_WEBSITE  := https://dbus.freedesktop.org/
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.12.2
+$(PKG)_CHECKSUM := 272bb5091770b047c8188b926d5e6038fa4fe6745488b2add96b23e2d9a83d88
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://$(PKG).freedesktop.org/releases/$(PKG)/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc expat
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://cgit.freedesktop.org/dbus/dbus/refs/tags' | \
+    $(SED) -n "s,.*<a href='[^']*/tag/?id=dbus-\\([0-9][^']*\\)'.*,\\1,p" | \
+    $(SORT) -V | \
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        $(MXE_CONFIGURE_OPTS) \
+        --disable-tests \
+        --disable-verbose-mode \
+        --disable-asserts \
+        --disable-maintainer-mode \
+        --disable-silent-rules \
+        --disable-launchd \
+        --disable-doxygen-docs \
+        --disable-xml-docs \
+        CFLAGS='-DPROCESS_QUERY_LIMITED_INFORMATION=0x1000'
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+endef
